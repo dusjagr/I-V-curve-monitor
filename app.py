59dfb41c03427947e6a2cefd80e8531be2bb38e9
dusjagr <marc@dusseiller.ch>
2025-03-06 23:25:59 +0100

    Add headers to support iframe embedding

--- app.py
+++ app.py
@@ -97,5 +97,11 @@ def get_data():
             'error': str(e)
         })
 
+@app.after_request
+def after_request(response):
+    response.headers.add('X-Frame-Options', 'ALLOW-FROM *')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
